4047fede681537382c37807e57611243d1d4b70a
useradmin <useradmin@DT01070403.technologic.com>
2019-04-11 16:58:06 +0530

    msql added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var mysql      = require('mysql');
 
 var express = require('express')
 var app = express()
@@ -8,5 +8,25 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.post('/authenticate', (req ,res)=>{
+
+	var connection = mysql.createConnection({
+  host     : 'db4free.net',
+  user     : 'prakhar',
+  password : '',
+  database : 'accmanager'
+});
+
+	connection.connect();
+
+	connection.query('SELECT * FROM ACCESS_STATUS', (error, results, fields)=>{
+		if(error)
+			console.log(error)
+		console.log(fields)
+	})
+
+	connection.end();
+
+})
 
 app.listen(process.env.PORT || 3001)
